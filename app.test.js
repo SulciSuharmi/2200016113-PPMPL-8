a25c18e9531b5204d84065f7cf068209f1afd709
SulciSuharmi <2200016113@webmail.uad.ac.id>
2024-12-23 22:47:14 +0700

    Update code

--- app.test.js
+++ app.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest');
 const app = require('./app');
 describe('GET /', () => {
-    it('responds with Hello, World!', async () => {
+    it('responds with Hello, World!!', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
-        expect(res.text).toBe('Hello, World!');
+        expect(res.text).toBe('Hello, World!!');
     });
 });
